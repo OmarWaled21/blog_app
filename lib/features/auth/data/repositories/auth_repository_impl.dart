e51f3fbe1852bc84a9e8f11956550495408af30c
OmarWaled21 <omar.waled484@gmail.com>
2025-02-05 23:11:54 +0200

    add auth exception in auth repository

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -44,11 +44,8 @@ Future<Either<Failure, UserModel>> _getUser(
   try {
     final user = await fn();
     return right(user);
-<<<<<<< HEAD
   } on AuthException catch (e) {
     return left(Failure(msg: e.message));
-=======
->>>>>>> 2b23ca92ac997e61ea64e469834d896f2a9aab7b
   } on ServerException catch (e) {
     return left(Failure(msg: e.msg));
   }
