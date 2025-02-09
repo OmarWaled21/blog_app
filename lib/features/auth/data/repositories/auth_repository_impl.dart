decf62714e0fff8b27fd05a6637f0fd63a8f5a37
OmarWaled21 <omar.waled484@gmail.com>
2025-02-09 23:01:03 +0200

    check the internet in sign up and login and current User

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/network/connection_checker.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/models/user_model.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
@@ -8,12 +9,30 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
+  final ConnectionChecker internetConnection;
 
-  AuthRepositoryImpl({required this.remoteDataSource});
+  AuthRepositoryImpl(
+      {required this.remoteDataSource, required this.internetConnection});
 
   @override
   Future<Either<Failure, UserModel>> currentUser() async {
     try {
+      if (!await internetConnection.isConnected()) {
+        final session = remoteDataSource.currentUserSession;
+
+        if (session == null) {
+          left(Failure(msg: 'User not logged in'));
+        }
+
+        return Right(
+          UserModel(
+            id: session!.user.id,
+            name: '',
+            email: session.user.email ?? '',
+          ),
+        );
+      }
+
       final user = await remoteDataSource.getCurrentUserData();
       if (user == null) {
         left(Failure(msg: 'User not logged in'));
@@ -51,17 +70,20 @@ class AuthRepositoryImpl implements AuthRepository {
       ),
     );
   }
-}
 
-Future<Either<Failure, UserModel>> _getUser(
-  Future<UserModel> Function() fn,
-) async {
-  try {
-    final user = await fn();
-    return right(user);
-  } on AuthException catch (e) {
-    return left(Failure(msg: e.message));
-  } on ServerException catch (e) {
-    return left(Failure(msg: e.msg));
+  Future<Either<Failure, UserModel>> _getUser(
+    Future<UserModel> Function() fn,
+  ) async {
+    try {
+      if (!await internetConnection.isConnected()) {
+        return Left(Failure(msg: 'No internet connection'));
+      }
+      final user = await fn();
+      return right(user);
+    } on AuthException catch (e) {
+      return left(Failure(msg: e.message));
+    } on ServerException catch (e) {
+      return left(Failure(msg: e.msg));
+    }
   }
 }
